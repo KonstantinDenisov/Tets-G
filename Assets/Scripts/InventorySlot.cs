7d97dbbb9dcabf666a0da53c41a4f28957120565
KonstantinDenisov <omacs@mail.ru>
2023-09-16 20:33:53 +0300

    Convert

--- Assets/Scripts/InventorySlot.cs
+++ Assets/Scripts/InventorySlot.cs
@@ -1,8 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class InventorySlot : MonoBehaviour
 {
    [SerializeField] private Item _item;
+   [SerializeField] private string _label;
+   [SerializeField] private int _int;
+   
+
+   private void Start()
+   {
+      _int = 10;
+      _label = Convert.ToString(_int);
+   }
 }
