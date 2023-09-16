d441bea30761da4c79036bf00ac8a5fe5fb1f715
KonstantinDenisov <omacs@mail.ru>
2023-09-16 19:49:53 +0300

    Create InventorySlot

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,9 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Item : MonoBehaviour
+public class Item : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     [SerializeField] private int _cost;
+    private Transform _parrenAfterDrag;
 
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _parrenAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End Drag");
+    }
 }
