8acfe30258dc0778684078105940384f3cae122d
KonstantinDenisov <omacs@mail.ru>
2023-09-17 15:17:24 +0300

    Create cost-label

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,11 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Item : MonoBehaviour
 {
     [SerializeField] private int _cost;
+    [SerializeField] private TextMeshProUGUI _label;
+
+    private void Start()
+    {
+        _label.text = Convert.ToString(_cost);
+    }
 
     public int Cost
     {
